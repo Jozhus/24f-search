b50325d76742c89205c9837994d018d184614cd7
Jozhus <vauit3@yahoo.com>
2022-02-11 23:49:59 -0800

    Fundamental frequency translated to pitch and displayed

--- src/Audio.ts
+++ src/Audio.ts
@@ -1,11 +1,27 @@
 const analyserOptions = {
 	fftSize: 32768, // 32768 max
-	smoothingTimeConstant: 0.5
+	smoothingTimeConstant: 0.25 //0.5
 }
 
 const streamOptions = {
 	video: false,
 	audio: true
+}
+
+const pitch = {
+	C: 523.25,
+	Db: 554.37,
+	D: 587.33,
+	Eb: 622.25,
+	E: 659.25,
+	F: 698.46,
+	Gb: 739.99,
+	G: 783.99,
+	Ab: 830.61,
+	A: 880.00,
+	Bb: 932.33,
+	B: 987.77,
+	C2: 1046.50
 }
 
 class Audio {
@@ -14,6 +30,7 @@ class Audio {
 	analyser: AnalyserNode | null;
 	canvas: HTMLCanvasElement | null;
 	enabled: boolean = false;
+	sampleRate: number = 0;
 	fundamentalFreq: number = 0;
 
     constructor(canvas: HTMLCanvasElement) {
@@ -38,6 +55,28 @@ class Audio {
 		this.enabled = !this.enabled;
 	}
 
+	private findNearestPitch(freq: number): string {
+		let result: string = "";
+
+		if (freq <= 0) {
+			return "";
+		} else if (freq < pitch.C) {
+			result = this.findNearestPitch(freq * 2);
+		} else if (freq > pitch.C2) {
+			result = this.findNearestPitch(freq / 2);
+		} else {
+			result = Object.entries(pitch).sort((a: [string, number], b: [string, number]) => {
+				if (Math.abs(a[1] - freq) < Math.abs(b[1] - freq)) {
+					return -1;
+				}
+
+				return 0;
+			})[0][0];
+		}
+
+		return result;
+	}
+
     private async initAnalyser(): Promise<void> {
 		if (navigator.mediaDevices.getUserMedia !== null) {
 			try {
@@ -46,6 +85,7 @@ class Audio {
 				this.analyser.fftSize = analyserOptions.fftSize;
 				this.analyser.smoothingTimeConstant = analyserOptions.smoothingTimeConstant;
 				audioCtx.createMediaStreamSource(await navigator.mediaDevices.getUserMedia(streamOptions)).connect(this.analyser);
+				this.sampleRate = audioCtx.sampleRate;
 				this.data = new Uint8Array(this.analyser.frequencyBinCount);
 			} catch (err) {
 				console.error("Failed to initialize analyser!", err);
@@ -86,20 +126,29 @@ class Audio {
 				let x = 0;
 				for(let i = 0; i < bufferLength; i++) {
 					let barHeight = this.data![i];
-					if(barHeight > max_val) {
+					if (barHeight > 100 && barHeight > max_val) {
 						max_val = barHeight;
 						max_index = i;
 					}
 
 					canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
 
-					canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+					canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
 					x += barWidth;
 				}
+
+				canvasCtx.strokeStyle = 'rgb(0, 255, 0)';
+				canvasCtx.strokeRect(0, HEIGHT-(max_val/2), WIDTH, 1);
+
+				canvasCtx.strokeStyle = 'rgb(255, 255, 0)';
+				canvasCtx.strokeRect(max_index * barWidth, 0, 1, HEIGHT);
 				
-				this.fundamentalFreq = 48000 * max_index / analyserOptions.fftSize;
+				this.fundamentalFreq = this.sampleRate * max_index / analyserOptions.fftSize;
+				canvasCtx.fillStyle = 'rgb(0, 0, 255)';
+				canvasCtx.font = '48px serif';
+  				canvasCtx.fillText(this.findNearestPitch(this.fundamentalFreq), WIDTH - 75, 50);
 			} catch (err) {
-				console.error("Failed to draw!", err);
+				console.error(`Failed to draw! ${this.fundamentalFreq}`, err);
 			}
 		}
 	}
